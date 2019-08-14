c590c1e37e25bad5a73cfc36d2363b22480fd0df
edwardhdez <edwardhdez21@gmail.com>
2019-08-14 16:37:22 -0400

    Header with title and search box

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+import './App.css'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchfield: event.target.value
+        })
+
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter((robots) => {
+            return robots.name
+                .toLowerCase()
+                .includes(this.state.searchfield.toLowerCase());
+        })
+
+        return (
+            <div className='tc'>
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+}
+
+export default App;
